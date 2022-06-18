d81b36916bc4ffa68811690087b10e2a74f9e94e
he77789 <>
2022-06-18 11:26:24 +0800

    basic framework, copy and flatten done

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ Q3.28 format is used in the output
 import numpy as np
 import onnx
 from onnx import numpy_helper
+import ophelper
 
 model = onnx.load('models/mnist_output_model.onnx')
 graph = model.graph
@@ -15,24 +16,54 @@ weights = []
 for tensor in graph.initializer:
   weights.append(numpy_helper.to_array(tensor))
 
-input_shape = weights[-2][2:3]
+input_shape = tuple(weights[-2][2:])
+print(input_shape)
 weights = weights[:-2]
+weight_order = [2,0,1]
 
 output = ""
 
+
+
 keys = ['image']
+unweighted_layers = 0
+last_shape = input_shape
 for layer, node in enumerate(graph.node):
-  print(node)
   weight_count = 0
   current_weights = []
+  
+  flatten = False
+  
   for t in node.input:
     if t.endswith('ReadVariableOp:0'): # weights
-      current_weights.append(weights[weight_count-2*(layer+1)])
+      current_weights.append(weights[weight_count-weight_order[layer-unweighted_layers]])
+      weight_count += 1
+    elif t.startswith('const_fold_opt'):
+      flatten = True
+    elif layer != 0: # load output of last layer to this layer
+      output += ophelper.opCopy(last_shape, 'scoreboard players operation #l{cn}_%d nn_eval = #l{pn}_%d nn_eval\n'.format(cn=layer,pn=layer-1), 'scoreboard players operation #l{cn}_%d_%d nn_eval = #l{pn}_%d_%d nn_eval\n'.format(cn=layer,pn=layer-1), 'scoreboard players operation #l{cn}_%d_%d_%d nn_eval = #l{pn}_%d_%d_%d nn_eval\n'.format(cn=layer,pn=layer-1))
+    
+  if weight_count == 0:
+    unweighted_layers += 1
+  
+  """
+  print(layer)
+  print(unweighted_layers)
+  print(current_weights)
+  """
   
   match node.op_type:
     case 'Reshape':
-      pass
+      if flatten:
+        output += ophelper.opFlatten(last_shape, 'scoreboard players operation #l{cn}_%d_%d nn_eval = #l{pn}_%d nn_eval\n'.format(cn=layer,pn=layer-1))
+        last_shape = [np.prod(last_shape)]
     case 'Conv':
-      print(node.attribute)
+      pass
+      # print(node.attribute)
     case _:
-      raise NotImplementedError(str(node.op_type) + ' is currently not supported')
+      pass
+      # raise NotImplementedError(str(node.op_type) + ' is currently not supported')
+  
+  output += '\n'
+
+#print(output)
---
+++ ophelper.py
@@ -0,0 +1,28 @@
+import numpy as np
+def opCopy(shape, out1d, out2d, out3d):
+  output = ''
+  match len(shape):
+    case 1:
+      for i in range(shape[0]):
+        output += out1d % (i, i)
+    case 2:
+      for i,j in np.ndindex(shape):
+        output += out2d % (i, j, i, j)
+    case 3:
+      for i,j,k in np.ndindex(shape):
+        output += out3d % (i, j, k, i, j ,k)
+  return output
+
+def opFlatten(shape, out):
+  output = ''
+  match len(shape):
+    case 1:
+      for i in range(shape[0]):
+        output += out % (i, i) # basically copy
+    case 2:
+      for i,j in np.ndindex(shape):
+        output += out % (i, j, j + i * shape[0])
+    case 3:
+      for i,j,k in np.ndindex(shape):
+        output += out % (i, j, k, k + j * shape[0] + i * shape[0] * shape[1])
+  return output
